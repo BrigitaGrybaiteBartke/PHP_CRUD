f89ca3756425bfdb6dd3f210305019d15a53d27f
BrigitaGrybaiteBartke <brigita.grybaite@gmail.com>
2022-09-22 19:41:26 +0300

    modify employees table logic

--- employees.php
+++ employees.php
@@ -1,9 +1,3 @@
-<?php
-    require_once "connect.php";
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projects</title>
+    <title>Employees</title>
+
     <!-- Bootstrap  -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
@@ -26,58 +21,43 @@
 
     <?php require_once "header.php"; ?>
 
+    <?php
+    // connection to server
+    $mysqli = new mysqli('localhost', 'root', '', 'crud') or die(mysqli_error($mysqli));
+    // selection
+    $result = $mysqli->query("SELECT * FROM employees") or die($mysqli->error);
+    ?>
 
     <div class="container">
-        <form action="" method="POST">
-            <div class="my-3 width">
-                <label for="name" class="form-label">Employee's Name</label>
-                <input type="text" name="name" placeholder="Enter employee name" class="form-control">
-            </div>
-            <div class="my-3 width">
-                <label for="email" class="form-label">Email</label>
-                <input type="text" name="email" placeholder="Enter employee email address" class="form-control">
-            </div>
+        <div class="mt-5">
+            <a href="newemployee.php" class="btn btn-primary">Add new Employee</a>
+        </div>
+        <div>
             <div>
-                <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                <h3 class="text-center mt-3 mb-4">Employee management system</h3>
             </div>
-        </form>
-    </div>
-
-    <div class="container">
-        <table class="table table-hover table-bordered shadow p-3 mb-3 bg-body rounded mt-5">
-            <tr class="table-secondary">
-                    <th>Id</th>
-                    <th>Employee Name</th>
-                    <th>Email</th>
-                    <th>Actions</th>
-            </tr>
-            <?php
-
-            $sql = "SELECT * FROM employees";
-            $result = mysqli_query($con, $sql);
-
-            if(mysqli_num_rows($result) > 0) {
-                while($row = mysqli_fetch_assoc($result)) {
-                    $id = $row['id'];
-                    $name = $row['name'];
-                    $email = $row['email'];
-
-                    echo "<tr>
-                        <td>$id</td>
-                        <td>$name</td>
-                        <td>$email</td>
+            <table class="table table-hover table-bordered shadow p-3 mb-3 bg-body rounded">
+                <tr>
+                    <td>Id</td>
+                    <td>Name</td>
+                    <td>Email</td>
+                    <td>Action</td>
+                </tr>
+                <?php if (mysqli_num_rows($result) > 0) while ($row = $result->fetch_assoc()) : ?>
+                    <tr>
+                        <td><?php echo $row['id']; ?></td>
+                        <td><?php echo $row['name']; ?></td>
+                        <td><?php echo $row['email']; ?></td>
                         <td>
-                            <a href=\"\" class=\"btn btn-outline-primary\">Edit</a>
-                            <a href=\"\" class=\"btn btn-outline-danger\">Delete</a>
+                            <a href="" class="btn btn-outline-primary">Edit</a>
+                            <a href="" class="btn btn-outline-danger">Delete</a>
                         </td>
-                    </tr>";
-                }
-            }
-
-            ?>
-
-        </table>
+                    </tr>
+                <?php endwhile; ?>
+            </table>
+        </div>
     </div>
+
 
 
 </body>
