d31e39e9a9e344a6f5f66fecd2f64c9a0e7c4b8a
BrigitaGrybaiteBartke <brigita.grybaite@gmail.com>
2022-09-26 20:25:28 +0300

    change connection

--- connect.php
+++ connect.php
@@ -1,16 +1,5 @@
 <?php
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "crud";
-
-    $con = mysqli_connect($servername, $username, $password, $dbname);
-    if(!$con) {
-        die(mysqli_connect_error());
-    } else {
-        echo "Connected successfully!";
-    }
-
+    $mysqli = new mysqli('localhost', 'root', '', 'crud') or die(mysqli_error($mysqli));
 
 ?>
